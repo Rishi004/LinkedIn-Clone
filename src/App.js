d44cf3382ec5022bec49a5e03c65fcd0bb10700a
Rishi004 <rishireiko@gmail.com>
2023-03-10 15:51:23 +0530

    10 of March 2023

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
 import Login from "./components/Login";
 
 function App() {
@@ -8,6 +10,15 @@ function App() {
             <Router>
                 <Routes>
                     <Route path="/" element={<Login />} />
+                    <Route
+                        path="/home"
+                        element={
+                            <>
+                                <Header />
+                                <Home />
+                            </>
+                        }
+                    />
                 </Routes>
             </Router>
         </div>
